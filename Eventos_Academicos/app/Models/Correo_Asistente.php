b4b8e1c2c26a128edb6552972e4877c95eff4f6a
Luis Adrian Silva Reyes <54382882+Luissf1@users.noreply.github.com>
2024-04-06 22:45:34 -0700

    tablas

---
+++ Eventos_Academicos/app/Models/Correo_Asistente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Correo_Asistente extends Model
+{
+    use HasFactory;
+}

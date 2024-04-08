3d4f77a40cf3682176b85add5507d1d0ac510667
Luis Adrian Silva Reyes <54382882+Luissf1@users.noreply.github.com>
2024-04-08 12:40:35 -0700

    Model Update
    2 models updated

--- Eventos_Academicos/app/Models/Afiliacion.php
+++ Eventos_Academicos/app/Models/Afiliacion.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Afiliacion extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'nombre_afiliacion'
+    ];
+
+    public function autor():HasMany{
+        return $this-> hasMany(Autor::class);
+    }
 }
--- Eventos_Academicos/app/Models/Asistente.php
+++ Eventos_Academicos/app/Models/Asistente.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Asistente extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'nombre_asistente',
+        'apellidopat_asistente',
+        'apellidomat_asistente',
+        'autor_id',
+    ];
+
+    public function autor():BelongsTo{
+        return $this-> belongsTo(Autor::class);
+    }
 }

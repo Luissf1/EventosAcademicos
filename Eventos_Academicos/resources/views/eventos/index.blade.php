d1b0cc39d2e182f64f6ecac5523ddeb1ec0c0ff8
Luis Adrian Silva Reyes <54382882+Luissf1@users.noreply.github.com>
2024-04-10 10:35:11 -0700

    Model

--- Eventos_Academicos/resources/views/eventos/index.blade.php
+++ Eventos_Academicos/resources/views/eventos/index.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
+        <div class="flex justify-between">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Evento') }}
+            {{ __('Eventos') }}
         </h2>
+        <div>
+            <a href="{{route('evento.create')}}" class="dark:text-white hover:text-slate-200">Evento Nuevo</a>
+        </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
